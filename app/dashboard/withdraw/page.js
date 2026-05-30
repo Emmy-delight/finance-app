f605c8ac6b89b1f5545c7761e43222c97ba7f7d3
Emmy-delight <165961039+Emmy-delight@users.noreply.github.com>
2026-05-30 15:44:50 +0100

    withdrawal page

--- app/dashboard/withdraw/page.js
+++ app/dashboard/withdraw/page.js
@@ -1,5 +1,40 @@
+import { Card, CardContent, CardHeader, TextField } from "@mui/material";
+
 export default function Withdraw (){
     return (
-        <main>Withdrawal page</main>
+        <main className="min-h-screen flex justify-center items-center px-20 py-10">
+            <Card sx={{width: 380, height: 300}}>
+                <CardHeader sx={{textAlign: "center"}}
+                title="Withdraw Funds"
+                subheader="Withdraw money from your account" />
+            
+              <CardContent>
+                  <form className="flex flex-col gap-3">
+                     <div>
+                       <TextField
+                         fullWidth
+                         size="small"
+                         id="amount"
+                         label="Withdrawal Amount"
+                         type="number"
+                         placeholder="Enter withdrawal amount"
+                       />
+                     </div>
+                     <div>
+                        <TextField
+                         fullWidth
+                         multiline
+                         rows={2}
+                         type="text"
+                         id="description"
+                         label="descripton"
+                         placeholder="Enter withdrawal notes"
+                        />
+                     </div>
+                     <button className="w-full h-10 bg-[#1D4ED8] text-white rounded-md cursor-pointer " type="submit">Withdraw Funds</button>
+                  </form>
+              </CardContent>
+          </Card>
+        </main>
     )
 }
